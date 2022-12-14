2094b75ac7925af1a454079a40ae922790897997
fresh88888888 <87177321+fresh88888888@users.noreply.github.com>
2022-12-14 23:06:06 +0800

    create web service restful

---
+++ tdd_item_4/src/main/java/org/ws/rs/tdd_item_4/HelloApplication.java
@@ -0,0 +1,9 @@
+package org.ws.rs.tdd_item_4;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
