b76aa6ded653c182164166bd652c3b5db6a9d273
fresh88888888 <87177321+fresh88888888@users.noreply.github.com>
2022-12-23 18:03:37 +0800

    restful api

--- tdd_item_4/src/main/java/org/ws/tdd/rest/ResourceServlet.java
+++ tdd_item_4/src/main/java/org/ws/tdd/rest/ResourceServlet.java
@@ -3,6 +3,9 @@ package org.ws.tdd.rest;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.ws.rs.core.MultivaluedMap;
+import jakarta.ws.rs.core.NewCookie;
+import jakarta.ws.rs.ext.RuntimeDelegate;
 
 public class ResourceServlet extends HttpServlet {
     private Runtime runtime;
@@ -15,5 +18,12 @@ public class ResourceServlet extends HttpServlet {
         ResourceRouter router = runtime.getResourceRouter();
         OutboundResponse outboundResponse = router.dispatch(req, runtime.createResourceContext(req, resp));
         resp.setStatus(outboundResponse.getStatus());
+        MultivaluedMap<String, Object> headers = outboundResponse.getHeaders();
+        for (String name: headers.keySet()) {
+            for (Object value: headers.get(name)) {
+                RuntimeDelegate.HeaderDelegate delegate = RuntimeDelegate.getInstance().createHeaderDelegate(value.getClass());
+                resp.addHeader(name, delegate.toString(value));
+            }
+        }
     }
 }
--- tdd_item_4/src/test/java/org/ws/tdd/rest/ResourceServletTest.java
+++ tdd_item_4/src/test/java/org/ws/tdd/rest/ResourceServletTest.java
@@ -2,12 +2,17 @@ package org.ws.tdd.rest;
 
 import jakarta.servlet.Servlet;
 import jakarta.ws.rs.container.ResourceContext;
+import jakarta.ws.rs.core.MultivaluedHashMap;
+import jakarta.ws.rs.core.MultivaluedMap;
+import jakarta.ws.rs.core.NewCookie;
 import jakarta.ws.rs.core.Response;
-import org.junit.jupiter.api.Assertions;
+import jakarta.ws.rs.ext.RuntimeDelegate;
 import org.junit.jupiter.api.Test;
 
 import java.net.http.HttpResponse;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 public class ResourceServletTest extends ServletTest{
@@ -28,18 +33,49 @@ public class ResourceServletTest extends ServletTest{
         return new ResourceServlet(runtime);
     }
 
-    //TODO: use status code as http status
     @Test
     public void should_use_status_from_response() throws Exception{
         OutboundResponse response = mock(OutboundResponse.class);
         when(response.getStatus()).thenReturn(Response.Status.NOT_MODIFIED.getStatusCode());
         when(router.dispatch(any(), eq(context))).thenReturn(response);
+        when(response.getHeaders()).thenReturn(new MultivaluedHashMap<>());
 
         HttpResponse<String> httpResponse = get("/test");
-        Assertions.assertEquals(httpResponse.statusCode(), Response.Status.NOT_MODIFIED.getStatusCode());
+        assertEquals(httpResponse.statusCode(), Response.Status.NOT_MODIFIED.getStatusCode());
 
     }
-    //TODO: use headers as http headers
+    @Test
+    public void should_use_http_headers_from_response() throws Exception{
+
+        RuntimeDelegate delegate = mock(RuntimeDelegate.class);
+        RuntimeDelegate.setInstance(delegate);
+
+        when(delegate.createHeaderDelegate(NewCookie.class)).thenReturn(new RuntimeDelegate.HeaderDelegate<>() {
+            @Override
+            public NewCookie fromString(String value) {
+                return null;
+            }
+
+            @Override
+            public String toString(NewCookie value) {
+                return value.getName() + "="+ value.getValue();
+            }
+        });
+
+        NewCookie sessionId = new NewCookie.Builder("SESSION_ID").value("session").build();
+        NewCookie userId = new NewCookie.Builder("USER_ID").value("user").build();
+        OutboundResponse response = mock(OutboundResponse.class);
+        MultivaluedMap<String, Object> headers = new MultivaluedHashMap<>();
+        headers.addAll("Set-Cookie", sessionId, userId);
+
+        when(response.getStatus()).thenReturn(Response.Status.NOT_MODIFIED.getStatusCode());
+        when(response.getHeaders()).thenReturn(headers);
+        when(router.dispatch(any(), eq(context))).thenReturn(response);
+
+        HttpResponse<String> httpResponse = get("/test");
+        assertArrayEquals(httpResponse.headers().allValues("Set-Cookie").toArray(String[]::new), new String[]{"SESSION_ID=session", "USER_ID=user"});
+
+    }
     //TODO: writer body using MessageBodyWriter
     //TODO: 500 if MessageBodyWriter not found
     //TODO: throw WebApplicationException with response, use response
