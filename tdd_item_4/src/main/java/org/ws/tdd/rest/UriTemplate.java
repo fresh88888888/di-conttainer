82e3057e3cf3a578994e0f1e7781ca793f6ffea9
fresh88888888 <87177321+fresh88888888@users.noreply.github.com>
2022-12-29 17:47:24 +0800

    TODO: comparing result, with match literal, variable and specific variable

--- tdd_item_4/src/main/java/org/ws/tdd/rest/UriTemplate.java
+++ tdd_item_4/src/main/java/org/ws/tdd/rest/UriTemplate.java
@@ -5,8 +5,6 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 interface UriTemplate {
-    Optional<MatchResult> match(String path);
-
     interface MatchResult extends Comparable<MatchResult> {
         String getMatched();
 
@@ -14,66 +12,84 @@ interface UriTemplate {
 
         Map<String, String> getMatchedPathParameters();
     }
+    Optional<MatchResult> match(String path);
 }
 
-class UriTemplateString implements UriTemplate {
-    public static final String DefaultVariablePattern = "([^/]+?)";
-    private static final String LeftBracket = "\\{";
-    private static final String RightBracket = "}";
-    private static final String VariableName = "\\w[\\w\\.-]*";
-    private static final String NonBracket = "[^\\{}]+";
-    private static final Pattern variable = Pattern.compile(LeftBracket + group(VariableName) + group(":" + group(NonBracket)) + "?" + RightBracket);
-    private static final int VariableNameGroup = 1;
-    private static final int VariablePatternGroup = 3;
+class PathTemplate implements UriTemplate {
     private Pattern pattern;
-    private List<String> variables = new ArrayList<>();
     private int VariableGroupStartFrom;
-
-    public UriTemplateString(String template) {
-        this.pattern = Pattern.compile(group(variable(template)) + "(/.*)?");
+    private PathVariables pathVariables = new PathVariables();
+    public PathTemplate(String template) {
+        this.pattern = Pattern.compile(group(pathVariables.template(template)) + "(/.*)?");
         VariableGroupStartFrom = 2;
     }
-
-    private static String group(String pattern) {
-        return "(" + pattern + ")";
-    }
-
-    private String variable(String template) {
-        return variable.matcher(template).replaceAll(result -> {
-            String pattern = result.group(VariablePatternGroup);
-            String variableName = result.group(VariableNameGroup);
-            if (variables.contains(variableName)) {
-                throw new IllegalArgumentException("duplicate variable " + variableName);
-            }
-            variables.add(variableName);
-            return pattern == null ? DefaultVariablePattern : group(pattern);
-        });
-    }
-
     @Override
     public Optional<MatchResult> match(String path) {
         Matcher matcher = pattern.matcher(path);
         if (!matcher.matches()) {
             return Optional.empty();
         }
-        return Optional.of(new PathMatchResult(matcher));
+        return Optional.of(new PathMatchResult(matcher, pathVariables));
     }
+    class PathVariables implements Comparable<PathVariables>{
+        public static final String DefaultVariablePattern = "([^/]+?)";
+        private static final String LeftBracket = "\\{";
+        private static final String RightBracket = "}";
+        private static final String VariableName = "\\w[\\w\\.-]*";
+        private static final String NonBracket = "[^\\{}]+";
+        private static final Pattern variable = Pattern.compile(LeftBracket + group(VariableName) + group(":" + group(NonBracket)) + "?" + RightBracket);
+        private static final int VariableNameGroup = 1;
+        private static final int VariablePatternGroup = 3;
+        private List<String> variables = new ArrayList<>();
+        private int specificPatternCount = 0;
 
+        private String template(String template) {
+            return variable.matcher(template).replaceAll(this::replace);
+        }
+
+        private String replace(java.util.regex.MatchResult result) {
+            String name = result.group(VariableNameGroup);
+            String pattern = result.group(VariablePatternGroup);
+            if (variables.contains(name)) {
+                throw new IllegalArgumentException("duplicate variable " + name);
+            }
+            variables.add(name);
+            if(pattern != null){
+                specificPatternCount++;
+                return group(pattern);
+            }
+            return DefaultVariablePattern;
+        }
+
+        public Map<String, String> extract(Matcher matcher){
+            Map<String, String> parameters = new HashMap<>();
+            for (int i = 0; i < pathVariables.variables.size(); i++) {
+                parameters.put(pathVariables.variables.get(i), matcher.group(VariableGroupStartFrom + i));
+            }
+            return parameters;
+        }
+        @Override
+        public int compareTo(PathVariables o) {
+            if(variables.size() > o.variables.size()){
+                return -1;
+            }
+            if(variables.size() < o.variables.size()){
+                return 1;
+            }
+            return Integer.compare(o.specificPatternCount, specificPatternCount);
+        }
+    }
     class PathMatchResult implements MatchResult {
         private int matchLiteralCount;
-        private int count;
+        private PathVariables pathVariables;
         private Matcher matcher;
-        private Map<String, String> parameters = new HashMap<>();
+        private Map<String, String> parameters;
 
-        public PathMatchResult(Matcher matcher) {
+        public PathMatchResult(Matcher matcher, PathVariables variables) {
             this.matcher = matcher;
-            this.count = matcher.groupCount();
-            this.matchLiteralCount = matcher.group(1).length();
-
-            for (int i = 0; i < variables.size(); i++) {
-                this.parameters.put(variables.get(i), matcher.group(VariableGroupStartFrom + i));
-                matchLiteralCount -= matcher.group(VariableGroupStartFrom + i).length();
-            }
+            this.pathVariables = variables;
+            this.parameters = variables.extract(matcher);
+            this.matchLiteralCount = matcher.group(1).length() - parameters.values().stream().map(String::length).reduce(0, Integer::sum);
         }
         @Override
         public String getMatched() {
@@ -81,7 +97,7 @@ class UriTemplateString implements UriTemplate {
         }
         @Override
         public String getRemaining() {
-            return matcher.group(count);
+            return matcher.group(matcher.groupCount());
         }
         @Override
         public Map<String, String> getMatchedPathParameters() {
@@ -90,10 +106,16 @@ class UriTemplateString implements UriTemplate {
         @Override
         public int compareTo(MatchResult o) {
             PathMatchResult result = (PathMatchResult) o;
-            if (matchLiteralCount > result.matchLiteralCount) {
+            if (matchLiteralCount > result.matchLiteralCount){
                 return -1;
             }
-            return 0;
+            if(matchLiteralCount < result.matchLiteralCount){
+                return 1;
+            }
+            return pathVariables.compareTo(result.pathVariables);
         }
+    }
+    private static String group(String pattern) {
+        return "(" + pattern + ")";
     }
 }
--- tdd_item_4/src/test/java/org/ws/tdd/rest/PathTemplateTest.java
+++ tdd_item_4/src/test/java/org/ws/tdd/rest/PathTemplateTest.java
@@ -6,17 +6,17 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class UriTemplateStringTest {
+public class PathTemplateTest {
 
     @Test
     public void should_return_empty_if_path_not_matched() {
-        UriTemplateString template = new UriTemplateString("/users");
+        PathTemplate template = new PathTemplate("/users");
 
         assertTrue(template.match("/orders").isEmpty());
     }
     @Test
     public void should_return_match_result_if_path_matched(){
-        UriTemplateString template = new UriTemplateString("/users");
+        PathTemplate template = new PathTemplate("/users");
         Optional<UriTemplate.MatchResult> match = template.match("/users/1");
 
         assertEquals("/users", match.get().getMatched());
@@ -25,7 +25,7 @@ public class UriTemplateStringTest {
     }
     @Test
     public void should_return_match_result_if_matched_with_variable(){
-        UriTemplateString template = new UriTemplateString("/users/{id}");
+        PathTemplate template = new PathTemplate("/users/{id}");
         Optional<UriTemplate.MatchResult> match = template.match("/users/1");
 
         assertEquals("/users/1", match.get().getMatched());
@@ -35,29 +35,46 @@ public class UriTemplateStringTest {
     }
     @Test
     public void should_return_empty_if_not_match_given_pattern(){
-        UriTemplateString template = new UriTemplateString("/users/{id:[0-9]+}");
+        PathTemplate template = new PathTemplate("/users/{id:[0-9]+}");
         assertTrue(template.match("/orders/id").isEmpty());
     }
     @Test
     public void should_extract_variable_value_by_given_pattern(){
-        UriTemplateString template = new UriTemplateString("/users/{id:[0-9]+}");
+        PathTemplate template = new PathTemplate("/users/{id:[0-9]+}");
         UriTemplate.MatchResult result = template.match("/users/1").get();
 
         assertEquals("1", result.getMatchedPathParameters().get("id"));
     }
     @Test
     public void should_throw_illegal_argument_exception_if_variable_redefined(){
-        assertThrows(IllegalArgumentException.class, () -> new UriTemplateString("/users/{id:[0-9]+}/{id}"));
+        assertThrows(IllegalArgumentException.class, () -> new PathTemplate("/users/{id:[0-9]+}/{id}"));
     }
-    //TODO: comparing result, with match literal, variable and specific variable
     @Test
     public void should_compare_for_match_literal(){
-        String path = "/users/1234";
-        UriTemplate smaller = new UriTemplateString("/users/1234");
-        UriTemplate larger = new UriTemplateString("/users/{id}");
+        assertSmaller("/users/1234", "/users/1234", "/users/{id}");
+    }
+    @Test
+    public void should_compare_match_variables_if_matched_literal_same(){
+        assertSmaller("/users/1234567890/order", "/{resources}/1234567890/{action}", "/users/{id}/order");
+    }
+    @Test
+    public void should_compare_specific_variables_if_matched_literal_variables_same(){
+        assertSmaller("/users/1", "/users/{id:[0-9]+}", "/users/{id}");
+    }
+    @Test
+    public void should_compare_equal_match_result(){
+        UriTemplate template = new PathTemplate("/users/{id}");
+        UriTemplate.MatchResult result = template.match("/users/1").get();
+
+        assertEquals(0, result.compareTo(result));
+    }
+    private static void assertSmaller(String path, String smallerTemplate, String largerTemplate) {
+        UriTemplate smaller = new PathTemplate(smallerTemplate);
+        UriTemplate larger = new PathTemplate(largerTemplate);
         UriTemplate.MatchResult lhs = smaller.match(path).get();
         UriTemplate.MatchResult rhs = larger.match(path).get();
 
         assertTrue(lhs.compareTo(rhs) < 0);
+        assertTrue(rhs.compareTo(lhs) > 0);
     }
 }
